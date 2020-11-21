4e2757d0bf21d240f74fc9bafebe376558c5dbbd
Alexey Efimov <alexeyefimov15@gmail.com>
2020-11-21 13:06:01 +0200

    Added insert function for users.

--- register.php
+++ register.php
@@ -15,13 +15,17 @@ require_once("includes/classes/Account.php");
         $password = FormSanitizer::sanitizeFormPassword($_POST["password"]);
         $password2 = FormSanitizer::sanitizeFormPassword($_POST["password2"]);
 
-        $account->register($firstName, 
-                            $lastName, 
-                            $username,
-                            $email, 
-                            $email2, 
-                            $password, 
-                            $password2);
+        $success = $account->register($firstName, 
+                                        $lastName, 
+                                        $username,
+                                        $email, 
+                                        $email2, 
+                                        $password, 
+                                        $password2);
+
+        if($success) {
+            header("Location: index.php");
+        }
     }
 ?>
 
