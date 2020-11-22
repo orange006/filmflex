d700a4f00ad925668d95696750965796eddb11ac
Alexey Efimov <alexeyefimov15@gmail.com>
2020-11-22 17:31:55 +0200

    Added preview video styles.

--- includes/header.php
+++ includes/header.php
@@ -16,6 +16,10 @@
 <head>
     <title>Welcome to FilmFlex</title>
     <link rel="stylesheet" type="text/css" href="assets/style/style.css" />
+
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/2812e58256.js" crossorigin="anonymous"></script>
+    <script src="assets/js/script.js"></script>
 </head>
 
 <body>
