38f337b5aae995229d7deacaaf45fe977085fe2f
orange006 <alexeyefimov15@gmail.com>
2021-05-05 12:27:18 +0300

    Added tv-shows, movies and category pages.

--- includes/classes/CategoryContainers.php
+++ includes/classes/CategoryContainers.php
@@ -1,7 +1,7 @@
-<?php 
+<?php
 class CategoryContainers {
-    private $con;
-    private $username;
+
+    private $con, $username;
 
     public function __construct($con, $username) {
         $this->con = $con;
@@ -10,7 +10,7 @@ class CategoryContainers {
 
     public function showAllCategories() {
         $query = $this->con->prepare("SELECT * FROM categories");
-
+        
         $query->execute();
 
         $html = "<div class='previewCategories'>";
@@ -22,9 +22,41 @@ class CategoryContainers {
         return $html . "</div>";
     }
 
+    public function showTVShowCategories() {
+        $query = $this->con->prepare("SELECT * FROM categories");
+        
+        $query->execute();
+
+        $html = "<div class='previewCategories'>
+                    <h1>TV Shows</h1>";
+
+        while($row = $query->fetch(PDO::FETCH_ASSOC)) {
+            $html .= $this->getCategoryHtml($row, null, true, false);
+        }
+
+        return $html . "</div>";
+    }
+
+    public function showMovieCategories() {
+        $query = $this->con->prepare("SELECT * FROM categories");
+        
+        $query->execute();
+
+        $html = "<div class='previewCategories'>
+                    <h1>Movies</h1>";
+
+        while($row = $query->fetch(PDO::FETCH_ASSOC)) {
+            $html .= $this->getCategoryHtml($row, null, false, true);
+        }
+
+        return $html . "</div>";
+    }
+    
     public function showCategory($categoryId, $title = null) {
         $query = $this->con->prepare("SELECT * FROM categories WHERE id=:id");
+        
         $query->bindValue(":id", $categoryId);
+        
         $query->execute();
 
         $html = "<div class='previewCategories noScroll'>";
@@ -44,10 +76,10 @@ class CategoryContainers {
             $entities = EntityProvider::getEntities($this->con, $categoryId, 30);
         }
         else if($tvShows) {
-            // get tv show entities
+            $entities = EntityProvider::getTVShowEntities($this->con, $categoryId, 30);
         }
         else {
-            // get movies entities
+            $entities = EntityProvider::getMoviesEntities($this->con, $categoryId, 30);
         }
 
         if(sizeof($entities) == 0) {
@@ -56,7 +88,7 @@ class CategoryContainers {
 
         $entitiesHtml = "";
         $previewProvider = new PreviewProvider($this->con, $this->username);
-
+        
         foreach($entities as $entity) {
             $entitiesHtml .= $previewProvider->createEntityPreviewSquare($entity);
         }
@@ -68,7 +100,7 @@ class CategoryContainers {
 
                     <div class='entities'>
                         $entitiesHtml
-                    </div> 
+                    </div>
                 </div>";
     }
 }
